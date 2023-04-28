ed84f808f59591d308ebc432cc2d35e794e4019c
Saketku98 <88224770+Saketku98@users.noreply.github.com>
2023-04-28 23:10:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Basic from './Component/Basic';
+import Counter from './Component/Counter';
+
+function App() {
+  return (
+    <div className="App">
+     <Basic/>
+     <Counter/>
+    </div>
+  );
+}
+
+export default App;
